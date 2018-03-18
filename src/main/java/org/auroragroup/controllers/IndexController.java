187f212bbafb25074ece66bf057734feaa968656
DESKTOP-FCMOFM8\Dmitry <dpoluektov@gmail.com>
2018-03-18 15:42:26 +0200

    index controller and view

---
+++ src/main/java/org/auroragroup/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package org.auroragroup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model m) {
+        m.addAttribute("someAttribute", "someValue");
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dmitry
+  Date: 3/18/2018
+  Time: 15:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<p>${someAttribute}</p>
+
+</body>
+</html>
